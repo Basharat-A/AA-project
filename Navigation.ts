09f146d5f68112a8069d43f9a3afbe341516423a
Basharat-A <145156374+Basharat-A@users.noreply.github.com>
2023-09-25 23:46:05 +0500

    Update Navigation.ts

--- Navigation.ts
+++ Navigation.ts
@@ -3,20 +3,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import options from '../screens/options';
-import details from '../screens/Fscreen';
+import Home from '../screens/Home';
 
 const MainStack = createStackNavigator();
-const MainStackScreen = () => {
-  return (
-    <MainStack.Navigator>
-      <MainStack.Screen name="options" component={options} />
-    </MainStack.Navigator>
-  );
-};
+const MainStackScreen = () => (
+  <MainStack.Navigator>
+    <MainStack.Screen
+      name="options"
+      component={options}
+      options={{headerShown: false}}
+    />
+    <MainStack.Screen name="details" component={Home} />
+    <MainStack.Screen name="Hotels" component={options} />
+  </MainStack.Navigator>
+);
 
 export default () => (
   <NavigationContainer>
     <MainStackScreen />
   </NavigationContainer>
 );
-//npm install @react-navigation/stack
